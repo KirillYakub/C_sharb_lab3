8bbc3b6198b980103fd1b0b526b5bf682b457e17
KirillYakub <kirillyakubchinskiy@gmail.com>
2020-04-19 18:58:13 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(82, 78);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(433, 463);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(633, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 79);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Create Matrix";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(511, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Необходимо выполнить операцию 2А*(А+B)-3A с матрицами А и В";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 572);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public int row = 3, col = 3, count = 0, count_1 = 0, A, A1, B, B1, C, C1, D, D1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void richTextBox1_TextChanged(object sender, EventArgs e)
+        { }
+
+        public void Form1_Load(object sender, EventArgs e)
+        { }
+
+        public void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += ($"Матрица A номер {++count}");
+
+            richTextBox1.Text += ("\n");
+
+            Random rand = new Random();
+
+            Matrix number = new Matrix();
+
+            int[,] arrA = new int[row, col];
+
+            int[,] arr_twoA = new int[row, col];
+
+            int[,] diff_of_arr = new int[row, col];
+
+            int[,] arrB = new int[row, col];
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    arrA[i, j] = rand.Next(0, 2);
+                    richTextBox1.Text += ($"{arrA[i, j]}" + "  ");
+                }
+                richTextBox1.Text += ("\n");
+            }
+
+            richTextBox1.Text += ("\n");
+
+            richTextBox1.Text += ("Удвоенная матрица A" + "\n");
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    arr_twoA[i, j] = number.TwoX_Matrix(arrA, i, j);
+                    richTextBox1.Text += ($"{arr_twoA[i, j]}" + "  ");
+                }
+                richTextBox1.Text += ("\n");
+            }
+
+            richTextBox1.Text += ("\n" + "Выполнена операция 2*A" + "\n\n" + $"Матрица В номер {++count_1}" + "\n");
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    arrB[i, j] = rand.Next(0, 2);
+                    richTextBox1.Text += ($"{arrB[i, j]}" + "  ");
+                }
+                richTextBox1.Text += ("\n");
+            }
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    arrA[i, j] = number.Half_of_Matrix(arrA, i, j);
+                }
+            }
+
+            richTextBox1.Text += ("\n" + "Выполняем операцию A + B" + "\n");
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    arrB[i, j] = number.Sum_of_Matrix(arrA, arrB, i, j);
+                    richTextBox1.Text += ($"{arrB[i, j]}" + "  ");
+                }
+                richTextBox1.Text += ("\n");
+            }
+
+            richTextBox1.Text += ("\n" + "Выполняем операцию 2*A*(A + B)" + "\n");
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    arrB[i, j] = number.Product_of_Matrix(arr_twoA, arrB, i, j);
+                    richTextBox1.Text += ($"{arrB[i, j]}" + "  ");
+                }
+                richTextBox1.Text += ("\n");
+            }
+
+            richTextBox1.Text += ("\n" + "Выполняем операцию 3*A" + "\n");
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    arrA[i, j] = number.Three_X_Matrix(arrA, i, j);
+                    richTextBox1.Text += ($"{arrA[i, j]}" + "  ");
+                }
+                richTextBox1.Text += ("\n");
+            }
+
+            richTextBox1.Text += ("\n" + "Выполняем вычитание (A+B)-3*A" + "\n");
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    diff_of_arr[i, j] = number.Diff_of_Matrix(arrA, arrB, i, j);
+                    richTextBox1.Text += ($"{diff_of_arr[i, j]}" + "  ");
+                    A = diff_of_arr[0, 0]; 
+                    A1 = diff_of_arr[0, 2];
+                    B = diff_of_arr[2, 2];
+                    B1 = diff_of_arr[2, 0];
+                    C = diff_of_arr[1, 0];
+                    C1 = diff_of_arr[0, 1];
+                    D = diff_of_arr[1, 2];
+                    D1 = diff_of_arr[2, 1];
+                }
+                richTextBox1.Text += ("\n");
+            }
+
+            richTextBox1.Text += ("\n");
+
+            int count2 = 0;
+
+            if (A == A1 && A1 == B && B == B1 && B1 == A)
+            {
+                if (C == C1 && C1 == D && D == D1 && D1 == C)
+                {
+                    count2++;
+                }
+            }
+
+            if (count2 != 0)
+            {
+                richTextBox1.Text += ("Финальная матрица транcпонированная" + "\n\n");
+            }
+
+            else
+            {
+                richTextBox1.Text += ("Финальная матрица не транcпонированная" + "\n\n");
+            }
+        }
+    }
+
+    public class Matrix
+    {
+        public int TwoX_Matrix(int[,] arrA, int i, int j)
+        {
+            return arrA[i,j] *= 2;
+        }
+
+        public int Sum_of_Matrix(int[,] arrA, int[,] arrB, int i, int j)
+        {
+            return arrA[i, j] + arrB[i, j];
+        }
+
+        public int Diff_of_Matrix(int[,] arrA, int[,] arrB, int i, int j)
+        {
+            return arrB[i, j] - arrA[i, j];
+        }
+
+        public int Product_of_Matrix(int[,] arr_twoA, int[,] arrB, int i, int j)
+        {
+            return arr_twoA[i, j] * arrB[i, j];
+        }
+
+        public int Half_of_Matrix(int[,] arrA, int i, int j)
+        {
+            return arrA[i, j] /= 2;
+        }
+
+        public int Three_X_Matrix(int[,] arrA, int i, int j)
+        {
+            return arrA[i, j] *= 3;
+        }
+
+    }
+}
